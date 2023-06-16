3660bc249a12ec42fc5b177a9b70fff2c5380635
blank-666 <71154969+blank-666@users.noreply.github.com>
2023-06-16 14:55:20 +0300

    test

--- routes/movies.js
+++ routes/movies.js
@@ -4,7 +4,7 @@ const router = app.Router();
 
 import statusCodes from "../constants.js";
 
-import { moviesCollection } from "../db/connect-mongo.mjs";
+import db, { moviesCollection } from "../db/connect-mongo.mjs";
 import {
   convertFilterObject,
   convertId,
@@ -41,11 +41,99 @@ router.get("/filters", async (req, res, next) => {
   }
 });
 
+const createDirectorsCollection = async () => {
+  console.log("try to create collection");
+  const aggregationPipeline = [
+    {
+      $unwind: "$directors",
+    },
+    {
+      $group: {
+        _id: "$directors",
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        name: "$_id",
+      },
+    },
+  ];
+
+  const aggregationResult = await moviesCollection
+    .aggregate(aggregationPipeline)
+    .toArray();
+  await db.createCollection("directors");
+  const directorsCollection = db.collection("directors");
+  await directorsCollection.insertMany(aggregationResult);
+};
+
+const replaceDirectorsProperty = async () => {
+  const movieCollection = db.collection("movies");
+  const directorCollection = db.collection("directors");
+
+  const directors = await directorCollection.find().toArray(); // fetch all directors first
+  const directorsByName = directors.reduce((map, director) => {
+    map[director.name] = director._id; // create a map of director name to id
+    return map;
+  }, {});
+
+  const movies = await movieCollection.find().toArray();
+  const bulkWriteOps = [];
+
+  for (let movie of movies) {
+    let director_ids = [];
+    if (Array.isArray(movie.directors)) {
+      // check if directors is an array
+      for (let directorName of movie.directors) {
+        if (directorsByName[directorName]) {
+          // lookup director id from the map
+          director_ids.push(directorsByName[directorName]);
+        }
+      }
+      // prepare bulk write operations
+      bulkWriteOps.push({
+        updateOne: {
+          filter: { _id: movie._id },
+          update: { $set: { director_ids: director_ids } },
+        },
+      });
+    }
+  }
+
+  if (bulkWriteOps.length > 0) {
+    await movieCollection.bulkWrite(bulkWriteOps); // perform bulk update
+  }
+};
+
+const deleteFirst100Movies = async () => {
+  try {
+    // Вибираємо перші 100 документів з колекції
+    const moviesToDelete = await db
+      .collection("movies")
+      .find()
+      .limit(100)
+      .toArray();
+
+    // Видаляємо вибрані документи
+    await db
+      .collection("movies")
+      .deleteMany({ _id: { $in: moviesToDelete.map((movie) => movie._id) } });
+
+    console.log("Перші 100 фільмів були успішно видалені.");
+  } catch (error) {
+    console.error("Сталася помилка при видаленні фільмів:", error);
+  }
+};
+
 router.get("/", async (req, res, next) => {
   try {
     const { query } = req;
 
     const { offset = 1, limit = 10, sort, filter, search } = query;
+
+    // await createDirectorsCollection();
+    // await replaceDirectorsProperty();
 
     const stages = [];
 
@@ -84,14 +172,97 @@ router.get("/:id", async (req, res, next) => {
     const { params } = req;
     const { id } = params;
 
-    const movie = await moviesCollection.findOne({
-      _id: convertId(id),
-    });
+    const findWithLookup = async (
+      id,
+      collectionName,
+      lookupCollectionName,
+      lookupField,
+      resultFields
+    ) => {
+      const collection = db.collection(collectionName);
+      const lookupResultField = lookupCollectionName;
+      const temporaryField = `${lookupResultField}_docs`;
 
-    if (!movie) throw new ErrorHandler(NOT_FOUND, "Movie not found.");
+      // const addFieldsObject = {};
+
+      // resultFields.forEach((field) => {
+      //   const fieldName = `${lookupResultField}.${field}`;
+      //   addFieldsObject[fieldName] = `$${temporaryField}.${field}`;
+      // });
+
+      // console.log("addFieldsObject", addFieldsObject);
+
+      if (!collection)
+        throw new ErrorHandler(NOT_FOUND, "Collection not found.");
+
+      const stages = [
+        { $match: { _id: convertId(id) } },
+        {
+          $lookup: {
+            from: lookupCollectionName,
+            localField: lookupField,
+            foreignField: "_id",
+            as: temporaryField,
+          },
+        },
+        // {
+        //   $addFields: addFieldsObject,
+        // },
+        {
+          $addFields: {
+            [lookupResultField]: `$${temporaryField}.name`,
+          },
+        },
+        {
+          $project: {
+            [temporaryField]: 0,
+            [lookupField]: 0,
+          },
+        },
+      ];
+
+      const result = await moviesCollection.aggregate(stages).toArray();
+
+      return result;
+    };
+
+    const movie = await findWithLookup(
+      id,
+      "movies",
+      "directors",
+      "director_ids",
+      ["name", "_id"]
+    );
+
+    // const stages = [
+    //   { $match: { _id: convertId(id) } },
+    //   {
+    //     $lookup: {
+    //       from: "directors",
+    //       localField: "director_ids",
+    //       foreignField: "_id",
+    //       as: "director_docs",
+    //     },
+    //   },
+    //   {
+    //     $addFields: {
+    //       directors: "$director_docs.name",
+    //     },
+    //   },
+    //   {
+    //     $project: {
+    //       director_docs: 0,
+    //       director_ids: 0,
+    //     },
+    //   },
+    // ];
+
+    // const movie = await moviesCollection.aggregate(stages).toArray();
+
+    if (!movie.length) throw new ErrorHandler(NOT_FOUND, "Movie not found.");
 
     res.status(OK).json({
-      movie: movie,
+      movie: movie[0],
     });
   } catch (e) {
     next(e);
